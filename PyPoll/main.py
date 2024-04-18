6c6c2dbcc1774f0a142939b041a38bae67e52505
squintero94 <squintero94@gmail.com>
2024-04-18 01:31:10 -0400

    Eight Commit - Successfully established path to election_CSV, Calculated Total Votes, & Printed first 3 lines

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,22 @@
 import os
 import csv
 
-election_csv = os.path.join("..", "Starter_Code 4", "PyBank", "Resources", "election_data.csv")
+election_csv = os.path.join("..", "Starter_Code 4", "PyPoll", "Resources", "election_data.csv")
+
+# Establish Counter / Capture Variables
+total_votes = 0
+
+# Open and read csv
+with open(election_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    
+    # Skip the header row if it exists
+    header = next(csv_reader)
+    
+    for row in csv_reader:
+        total_votes = total_votes + 1
+
+# Print the Result to the Terminal
+    print("Election Results")
+    print("----------------------------")
+    print(f"Total Votes: {total_votes}")
