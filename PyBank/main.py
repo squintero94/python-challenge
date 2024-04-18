d192d3c388c81d85ece0408e2915402223c8bef9
squintero94 <squintero94@gmail.com>
2024-04-18 00:22:20 -0400

    Fifth Commit - Successfully calculated and printed results for Total and Average Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,9 @@ budget_csv = os.path.join("..", "Starter_Code 4", "PyBank", "Resources", "budget
 
 # Counter Variable for Total Months
 total_months = 0
+total_profit_losses = 0
+changes_in_profit_losses = []
+previous_profit_loss = 0
 
 # Open and read csv
 with open(budget_csv) as csv_file:
@@ -15,8 +18,21 @@ with open(budget_csv) as csv_file:
     
     for row in csv_reader:
         total_months = total_months + 1
+        profit_loss = int(row[1])
+        total_profit_losses += profit_loss
+        current_profit_loss = int(row[1])
+        if previous_profit_loss != 0:
+            change = current_profit_loss - previous_profit_loss
+            changes_in_profit_losses.append(change)
+        previous_profit_loss = current_profit_loss
+    average_change = 0
+    if len(changes_in_profit_losses) > 0:
+        average_change = sum(changes_in_profit_losses) / len(changes_in_profit_losses)
+
 
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
+    print(f"Total: ${total_profit_losses}")
+    print(f"Average Change: ${average_change:.2f}")
 
