0a4dbd8d631294f83a6ac350296c86825de37bd0
squintero94 <squintero94@gmail.com>
2024-04-18 01:54:11 -0400

    Ninth Commit - Calculated and Printed results of votes by Candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,18 +5,31 @@ election_csv = os.path.join("..", "Starter_Code 4", "PyPoll", "Resources", "elec
 
 # Establish Counter / Capture Variables
 total_votes = 0
+CCS_votes = 0
+DD_votes = 0
+RAD_votes = 0
 
 # Open and read csv
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-    
+
     # Skip the header row if it exists
     header = next(csv_reader)
-    
+
     for row in csv_reader:
-        total_votes = total_votes + 1
+        total_votes += 1
+        if row[2] == "Charles Casper Stockham":
+            CCS_votes += 1
+        elif row[2] == "Diana DeGette":
+            DD_votes += 1
+        elif row[2] == "Raymon Anthony Doane":
+            RAD_votes += 1
 
 # Print the Result to the Terminal
-    print("Election Results")
-    print("----------------------------")
-    print(f"Total Votes: {total_votes}")
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------")
+print(f"Charles Casper Stockham: {CCS_votes}")
+print(f"Diana DeGette: {DD_votes}")
+print(f"Raymon Anthony Doane: {RAD_votes}")
